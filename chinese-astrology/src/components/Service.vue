62db363259a677bef11b0f3b2bd6a17693c9f77c
pavelbekarev <bekarev.ps@gmail.com>
2024-07-31 13:20:52 +0500

    commit

---
+++ chinese-astrology/src/components/Service.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    name: String,
+    description: String,
+    imagePath: String
+})
+</script>
+
+
+<template>
+    <div v-for="service of services" :key="service.name" class="service__item-wrapper">
+        <img class="service__item-image" :src="service.imagePath" alt="">
+        <h3 class="service__item-h3">{{ service.name }}</h3>
+        <p class="service__item-description">
+          {{ service.description }}
+        </p>
+    </div>
+</template>
---
+++ chinese-astrology/src/components/ServiceList.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Service from './Service.vue';
+    
+
+</script>
+
+
+<template>
+    <Service 
+        name="Любовь в 2024 году"
+        description="У каждого из нас бывают периоды, когда в нашей карте Бацзы затрагивается Дом БракаДом Брака — второй столп в карте под названием «День» или «Личность»"
+        image-path="../../img/love.jpg"
+    />
+</template>
+
